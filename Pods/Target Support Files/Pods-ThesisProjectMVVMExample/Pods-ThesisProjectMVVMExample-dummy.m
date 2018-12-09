6893afe50bd430178a40f01474f7eebd1c822968
Maciej Hełmecki <mhelmecki@gmail.com>
2018-12-09 20:37:49 +0100

    First commit

---
+++ Pods/Target Support Files/Pods-ThesisProjectMVVMExample/Pods-ThesisProjectMVVMExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ThesisProjectMVVMExample : NSObject
+@end
+@implementation PodsDummy_Pods_ThesisProjectMVVMExample
+@end
---
+++ ThesisProjectMVVMExampleTests/ThesisProjectMVVMExampleTests.swift
@@ -0,0 +1,27 @@
+//
+//  ThesisProjectMVVMExampleTests.swift
+//  ThesisProjectMVVMExampleTests
+//
+//  Created by Maciej Hełmecki on 09/12/2018.
+//  Copyright © 2018 Maciej Hełmecki. All rights reserved.
+//
+
+@testable import ThesisProjectMVVMExample
+import XCTest
+
+public class ThesisProjectMVVMExampleTests: XCTestCase {
+
+    override public func setUp() {
+        super.setUp()
+    }
+
+    override public func tearDown() {
+        super.tearDown()
+    }
+
+    public func testExample() {}
+
+    public func testPerformanceExample() {
+        self.measure {}
+    }
+}
